3cff411aa0e66d01e0b274ac52589caa9e205e61
Josué  Rufino <73407358+JosueRufino@users.noreply.github.com>
2024-11-05 23:47:58 +0100

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-electron', '@nuxtjs/i18n', '@nuxtjs/color-mode'],
-  electron: {
-    // Configure aqui as opções do Electron, se necessário
-    // Por exemplo, se você quiser personalizar o caminho do main.js
-    // main: 'diretorio/main.js', // Adicione o caminho se necessário
-  },
+  modules: ['@nuxtjs/i18n', '@nuxtjs/color-mode'],
 });
---
+++ pages/auth/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>auth</h1>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "auth"
+})
+</script>
